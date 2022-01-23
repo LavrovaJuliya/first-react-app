77b3e747d815810245c01b5c2e7d755e997b4b31
Konstantine Brit <brit111001@gamil.com>
2022-01-23 12:53:13 +0200

    fix of Routies issue

--- src/App.js
+++ src/App.js
@@ -5,27 +5,23 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import News from './components/News/News';
 
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className='app-wrapper'>
+    <div className='app-wrapper'> 
         <Header />
         <Navbar />
-
-        <div className='app-wrapper-content' >
-        <Routes>
-          <Route path = './profile' element = {<Profile/>} />
-          <Route path = './dialogs' component = {Dialogs} />
-          <Route path = './news' component = {News} />
-         </Routes> 
-        </div>
-
-      </div>
-    </BrowserRouter >
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div> 
+    </div>
   );
 }
 
